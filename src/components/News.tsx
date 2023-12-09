2e6da16ac5873d3e255077e9c2434db98b709bcd
jsontype <jsontyper@gmail.com>
2023-12-09 16:27:34 +0900

    fix: 필요없는 주석제거

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -13,11 +13,9 @@ export default function News() {
   const [news, setNews] = useState([]);
 
   useEffect(() => {
-    // fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
     fetch("https://api.hnpwa.com/v0/news.json")
       .then((res) => res.json())
       .then((json) => setNews(json));
-    // .then(json => setNews(json))
   }, []);
 
   const render = () => {
